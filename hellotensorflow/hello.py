a051c22f3d773c053de6e7a35270ab22116099d6
Josh Peak <neozenith.dev@gmail.com>
2018-09-11 15:40:53 +1000

    experimenting still

--- hellotensorflow/hello.py
+++ hellotensorflow/hello.py
@@ -2,11 +2,13 @@
     author: Josh Peak
     description: learning tensorflow API
 """
-
+import os
 import tensorflow as tf
+from tensorflow import keras
 
 
 def default():
+    print("KERAS %s" % keras.__version__)
     hello = tf.constant('Hello, TensorFlow!')
     #sess = tf.Session()
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
@@ -26,12 +28,13 @@ def mnist_classification():
         tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(10, activation=tf.nn.softmax)
     ])
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+                  loss=tf.keras.losses.sparse_categorical_crossentropy,
                   metrics=['accuracy'])
 
     model.fit(x_train, y_train, epochs=5)
-    model.evaluate(x_test, y_test)
+    model.save_weights('mymodel.h5', save_format='h5')
+    print(model.evaluate(x_test, y_test))
 
 
 if __name__ == "__main__":
